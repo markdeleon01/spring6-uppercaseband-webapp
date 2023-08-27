8beacc1f9321a685b7817df919a2f6e07f3668f6
Mark de Leon <mark_deleon@hotmail.com>
2023-08-27 15:38:46 -0400

    Initial commit

---
+++ src/test/java/com/uppercaseband/uppercaseband/Spring6UppercaseBandWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uppercaseband.uppercaseband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6UppercaseBandWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
