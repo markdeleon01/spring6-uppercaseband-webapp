c79d83752829752d34bde9ad288599e4fc28d2e1
Mark de Leon <mark_deleon@hotmail.com>
2023-08-29 01:31:36 -0400

    Assert mapper functionalities

---
+++ src/test/java/com/uppercaseband/mappers/ArticleMapperTest.java
@@ -0,0 +1,71 @@
+package com.uppercaseband.mappers;
+
+import com.uppercaseband.domain.Article;
+import com.uppercaseband.domain.Category;
+import com.uppercaseband.domain.Media;
+import com.uppercaseband.domain.MediaType;
+import com.uppercaseband.model.ArticleDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ContextConfiguration(classes = {
+        ArticleMapperImpl.class,
+        MediaMapperImpl.class})
+class ArticleMapperTest {
+
+    static final String TITLE = "someTitle";
+    static final String DESCRIPTION = "someDescription";
+    static final String PATH = "images/image.png";
+
+    @Autowired
+    ArticleMapper articleMapper;
+
+    @Test
+    void articleToArticleDTO() throws Exception {
+        assertNotNull(articleMapper);
+
+        //given
+        Article article = new Article();
+        article.setId(1L);
+        article.setTitle(TITLE);
+        article.setDescription(DESCRIPTION);
+        article.setDisplayOrder(100);
+        article.setCategory(Category.HIGHLIGHTS);
+        Media media = new Media();
+        media.setId(1L);
+        media.setType(MediaType.IMAGE);
+        media.setPath(PATH);
+        article.setMedia(media);
+
+        //when
+        ArticleDTO articleDto = articleMapper.articleToArticleDTO(article);
+
+        //then
+        assertEquals(article.getTitle(), articleDto.getTitle());
+        assertEquals(article.getDescription(), articleDto.getDescription());
+        assertEquals(article.getDisplayOrder().intValue(), articleDto.getDisplayOrder());
+        assertEquals(article.getCategory().name(), articleDto.getCategory());
+        assertEquals(article.getMedia().getType().name(), articleDto.getMedia().getType());
+        assertEquals(article.getMedia().getPath(), articleDto.getMedia().getUrl());
+    }
+
+    @Test
+    void articleDTOToArticle() throws Exception {
+
+        //given
+        ArticleDTO articleDTO = new ArticleDTO();
+        articleDTO.setTitle(TITLE);
+
+        //when
+        Article article = articleMapper.articleDTOToArticle(articleDTO);
+
+        //then
+        assertEquals(articleDTO.getTitle(), article.getTitle());
+    }
+}
